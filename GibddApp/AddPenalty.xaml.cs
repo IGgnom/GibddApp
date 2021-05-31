be36d8ad1f2d918f9aa5d5be068a0fb8ae4c2cd7
IGgnom <timosha020702@yandex.ru>
2021-06-01 00:21:30 +0300

    Добавьте файлы проекта.

---
+++ GibddApp/AddPenalty.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using static GibddApp.EFAppData;
+
+namespace GibddApp
+{
+    public partial class AddPenalty : Window
+    {
+        public AddPenalty()
+        {
+            InitializeComponent();
+            violationComboBox.ItemsSource = AppDataEntities.Violation.ToList();
+            TPUComboBox.ItemsSource = AppDataEntities.TPU.ToList();
+            violationComboBox.SelectedItem = violationComboBox.Items[0];
+            TPUComboBox.SelectedItem = TPUComboBox.Items[0];
+        }
+
+        private void AddPenaltyButtonClick(object sender, RoutedEventArgs e)
+        {
+            if (string.IsNullOrEmpty(VINTextBox.Text) || string.IsNullOrEmpty(addressTextBox.Text) || string.IsNullOrEmpty(priceTextBox.Text))
+            {
+                MessageBox.Show($"Необходимо заполнить поля: {(string.IsNullOrEmpty(VINTextBox.Text) ? "\"VIN автомобиля\"" : "")}{(string.IsNullOrEmpty(addressTextBox.Text) ? "," : "")}{(string.IsNullOrEmpty(addressTextBox.Text) ? " \"Адрес совершенного правонарушения\"" : "")}{(string.IsNullOrEmpty(priceTextBox.Text) ? "," : "")}{(string.IsNullOrEmpty(priceTextBox.Text) ? " \"Стоимость\"" : "")}.", "Необходимо заполнить все поля!", MessageBoxButton.OK, MessageBoxImage.Warning);
+            }
+        }
+
+        private void RejectPenaltyButtonClick(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ GibddApp/MainWindow.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace GibddApp
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void SearchButtonClick(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void PayPenaltyButtonClick(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void AddPenaltyButtonClick(object sender, RoutedEventArgs e)
+        {
+            AddPenalty addPenalty = new AddPenalty();
+            addPenalty.ShowDialog();
+        }
+
+        private void EditPenaltyButtonClick(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
